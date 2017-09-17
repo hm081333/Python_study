ef6b5b1230ef73a6a96f8aefb66506bf52a69ecf
hm081333 <522751485@qq.com>
2017-09-18 00:26:00 +0800

    study hard

--- lib/dir_test.py
+++ lib/dir_test.py
@@ -1,35 +1,34 @@
 # Encoding: UTF-8
 import os
 
-
-def dir_or_file(path):
-	if (os.path.isdir(path)):
-		# print path
-		ly_path(path)
-		pass
-	elif (os.path.isfile(path)):
-		# print os.path.dirname(path)
-		if (os.path.dirname(path) == study_py_path):
-			# print path
-			print os.path.basename(path)
-			pass
-		else:
-			# print '--', path
-			print '--', os.path.basename(path)
-			pass
+from ly import zhcn
 
 
-def ly_path(path):
-	# print os.walk(path)
-	for f in os.walk(path):
-		print f
-		# all_path_file = os.path.join(path, '?')
-		# print glob.glob(all_path_file)
-		# for p in glob.glob(all_path_file):
-		#     dir_or_file(p)
-		#     pass
+def ly_file(path, counts):
+    if os.path.exists(path) == False:
+        zhcn('不存在此目录：' + path)
+        return
+    if os.path.isfile(path):
+        print '|-' + '---' * counts + ' ' + os.path.basename(path)
+        pass
+    elif os.path.isdir(path):
+        print '|-' + '---' * counts + ' ' + path
+        dept_paths = os.listdir(path)
+        for dept_path in dept_paths:
+            # 不推荐直接使用路径分隔符号 '\' '/' 推荐使用os.sep
+            # if path.rfind(os.path.sep) == len(path) - 1:
+            #     dept_path = path + dept_path
+            #     pass
+            # else:
+            #     dept_path = path + os.path.sep + dept_path
+            #     pass
+            dept_path = os.path.join(path, dept_path)
+            ly_file(dept_path, counts + 1)
+            pass
+    pass
 
 
-study_py_path = 'D:\\PY'
-
-ly_path(study_py_path)
+# study_py_path = 'D:\\PY'
+# print study_py_path
+zhcn('请输入路径')
+ly_file(raw_input(), 0)
